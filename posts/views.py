bfbb845d0a9a924e4f15194b43a7b51a4d5af1a9
Aleksei <axe455990@mail.ru>
2020-12-16 09:12:03 +0700

    My first commit

---
+++ posts/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Group
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm
+
+
+def index(request):
+    latest = Post.objects.all()[:11]
+    return render(request, "index.html", {"posts": latest})
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).all()[:12]
+    return render(request, "group.html", {"group": group, "posts": posts})
+
+
+@login_required
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['text']
+            group = form.cleaned_data['group']
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('index')
+    form = PostForm()
+    return render(request, 'new.html', {'form': form})
